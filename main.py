db53b23a281f51d1c987a4557d31ad7cba9dc63c
RubberTurtleDuck <112665725+RubberTurtleDuck@users.noreply.github.com>
2022-09-14 00:26:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,25 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-letter_to_index = dict(zip(alphabet, range(len(alphabet))))
-index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+is_over = False
+while not is_over:
+    direction = input("Write 'encode' if you wanna encrypt, and 'decode' if you wanna decrypt:  ")
+    text = input("Write a message:  ").lower()
+    shift = int(input("Write a shift number:  ")) % 26
 
-
-def encrypt(message, shift):
-    encrypted_text = ""
-    for letter in message:
-        if letter == " ":
-            shifted_letter = " "
-            encrypted_text += shifted_letter
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
+    for character in text:
+        if character in alphabet:
+            character_position = alphabet.index(character)
+            new_character_position = character_position + shift
+            end_text += alphabet[new_character_position]
         else:
-            index = (letter_to_index[letter]) + shift % len(alphabet)
-            shifted_letter = index_to_letter[index]
-            encrypted_text += shifted_letter
-    return encrypted_text
-
-
-def decrypt(ciphered_text, shift):
-    decrypted_text = ""
-    for letter in ciphered_text:
-        if letter == " ":
-            unshifted_letter = " "
-            decrypted_text += unshifted_letter
-        else:
-            index = (letter_to_index[letter]) - shift % len(alphabet)
-            shifted_letter = index_to_letter[index]
-            decrypted_text += shifted_letter
-    return decrypted_text
-
-
-on = True
-while on:
-    user_choice = input("E encrypt message. / D to decrypt message. / Write 'exit' to close the program: ").lower()
-
-    if user_choice == "e":
-        text = input("Write the message you want to encrypt: ").lower()
-        shift_number = int(input("Write how many slots you want to shift the cipher: "))
-        encrypted_message = encrypt(text, shift_number)
-        print(encrypted_message)
-    elif user_choice == "d":
-        text = input("Write the message you want to decrypt: ").lower()
-        shift_number = int(input("Write how many slots you want to shift the cipher: "))
-        encrypted_message = decrypt(text, shift_number)
-        print(encrypted_message)
-    elif user_choice == "exit":
-        on = False
+            end_text += character
+    print(end_text)
+    restart = input("Do you wanna go again?  ")
+    if restart == "no":
+        is_over = True
+        print("Goodbye! :D")
